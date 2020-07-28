692905ddc53cc638070b22dfe1c03dd909055774
areee <10089872+areee@users.noreply.github.com>
2020-07-28 20:30:45 +0300

    3.4 ready (deleting a person)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { response } = require('express')
 const app = express()
 
 app.use(express.json())
@@ -46,6 +47,12 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
 })
 
 const PORT = 3001
