097d1d4eab25f85b08baad4f01b775b99cfef120
areee <10089872+areee@users.noreply.github.com>
2020-08-01 20:54:52 +0300

    uibuild

---
+++ build/precache-manifest.1679eaecc369d8b8094b7dec22829700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e517a8a48386c1e1a232d7fb2c95eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41d199d199e7146eebd",
+    "url": "/static/css/main.8a5403ce.chunk.css"
+  },
+  {
+    "revision": "c574ff2531d61c703e37",
+    "url": "/static/js/2.16e154f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16e154f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41d199d199e7146eebd",
+    "url": "/static/js/main.41553deb.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
