31d5096ebc60cd7879029528495123071f475399
areee <10089872+areee@users.noreply.github.com>
2020-08-11 19:51:56 +0300

    3.15 ready (removing a person)

--- index.js
+++ index.js
@@ -65,9 +65,11 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+    // tähän vielä virheidenkäsittely catchilla
 })
 
 app.post('/api/persons', (req, res) => {
